9158733a217c3924cb23107890eb7f3a373f9ee1
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-30 20:48:30 +0530

    Update __main__.py

--- RestrictedBot/__main__.py
+++ RestrictedBot/__main__.py
@@ -1,43 +1,24 @@
-from pyrogram import Client, filters, idle
-import re
 import asyncio
-from os import getenv
-import os
-from pyrogram import __version__ as pyr
 import importlib
-from RestrictedBot import app
+from pyrogram import idle
+from RestrictedBot import LOGGER, app
 from RestrictedBot.plugins import ALL_MODULES
 
-#Logging...
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-
-# Pyrogram client
-app = Client(
-             name="RestrictedBot", 
-             api_id=API_ID, 
-             api_hash=API_HASH, 
-             bot_token=BOT_TOKEN,
-)
 
 async def app_boot():
     try:
         await app.start()
+    except Exception as ex:
+        LOGGER.error(ex)
+        quit(1)
+
     for all_module in ALL_MODULES:
         importlib.import_module("RestrictedBot.plugins" + all_module)
-    LOGGER("RestrictedBot.plugins").info("Successfully Imported Modules...")
-    LOGGER("RestrictedBot").info(
-        "Bot Started Successfully"
-    )
+
+    LOGGER.info(f"@{app.username} Started.")
     await idle()
-    await app.stop()
-    LOGGER("RestrictedBot").info("Stopping Bot...")
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
+    asyncio.get_event_loop().run_until_complete(anony_boot())
+    LOGGER.info("Stopping Bot...")

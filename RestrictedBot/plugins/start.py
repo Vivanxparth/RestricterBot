0c779482d9c77032478641a29641ace28d3882f9
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 16:35:01 +0530

    Update start.py

--- RestrictedBot/plugins/start.py
+++ RestrictedBot/plugins/start.py
@@ -1,11 +1,9 @@
-import asyncio
-from pyrogram.errors.exceptions.flood_420 import FloodWait
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton
+from pyrogram import *
+from pyrogram.types import *
 from RestrictedBot import app
 
 @app.on_message(filters.command("start") & filters.private)
-async def hello(app, message):
+async def start_command(client, message: Message):
   await message.reply_photo(
                         photo=f"https://telegra.ph/file/e41caec77a264f14e43d0.jpg",
                         caption=f"**ʜᴇʏ, {message.from_user.mention}\n\n๏ ᴛʜɪꜱ ɪꜱ ᴀ ᴍᴏꜱᴛ ᴘᴏᴡᴇʀꜰᴜʟ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ ɪᴛ ʜᴀꜱ ᴀ ᴀʙɪʟɪᴛʏ ᴛᴏ ᴅᴇʟᴇᴛᴇ ᴜɴᴡᴀɴᴛᴇᴅ ᴏʀ ᴜ ᴄᴀɴ ꜱᴀʏ ᴄᴏᴘʏʀɪɢʜᴛ ᴄᴏɴᴛᴇɴᴛ ɪᴛ ᴡɪʟʟ ᴄʟᴇᴀɴ ᴛʜᴏꜱᴇ ᴍᴇꜱꜱᴀɢᴇꜱ ᴡɪᴛʜ ɪɴ ᴀ ꜱᴇᴄᴏɴᴅ'ꜱ ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ꜱᴀᴠᴇ ⚠️ ʏᴏᴜʀ ɢʀᴏᴜᴘ ꜰʀᴏᴍ ᴄᴏᴘʏʀɪɢʜᴛ ɪɴꜰʀɪɴɢᴇᴍᴇɴᴛ ɪ ꜱᴜɢɢᴇꜱᴛᴇᴅ ʏᴏᴜ ᴛᴏ ᴀᴅᴅ ᴍᴇ ɪɴᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ.**",

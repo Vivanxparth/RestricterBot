63704b4e5b4bb5e55039a88dbced4cf075d4e97d
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 15:21:50 +0530

    Update start.py

--- RestrictedBot/plugins/start.py
+++ RestrictedBot/plugins/start.py
@@ -5,14 +5,14 @@ from RestrictedBot import app
 @app.on_message(filters.command("start") & filters.private)
 async def hello(app, message):
   await message.reply_photo(
-                        photo=f"https://telegra.ph/file/1912e94ef5c9b2985ec8c.jpg",
-                        caption=f"Hey, {message.from_user.mention}\n\n๏ This is a most powerful telegram bot it has a ability to delete unwanted or u can say copyright content it will clean those messages with in a second's if you want to save ⚠️ your group from copyright infringement I suggested you to add me into your group.",
+                        photo=f"https://telegra.ph/file/e41caec77a264f14e43d0.jpg",
+                        caption=f"ʜᴇʏ, {message.from_user.mention}\n\n๏ ᴛʜɪꜱ ɪꜱ ᴀ ᴍᴏꜱᴛ ᴘᴏᴡᴇʀꜰᴜʟ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ ɪᴛ ʜᴀꜱ ᴀ ᴀʙɪʟɪᴛʏ ᴛᴏ ᴅᴇʟᴇᴛᴇ ᴜɴᴡᴀɴᴛᴇᴅ ᴏʀ ᴜ ᴄᴀɴ ꜱᴀʏ ᴄᴏᴘʏʀɪɢʜᴛ ᴄᴏɴᴛᴇɴᴛ ɪᴛ ᴡɪʟʟ ᴄʟᴇᴀɴ ᴛʜᴏꜱᴇ ᴍᴇꜱꜱᴀɢᴇꜱ ᴡɪᴛʜ ɪɴ ᴀ ꜱᴇᴄᴏɴᴅ'ꜱ ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ꜱᴀᴠᴇ ⚠️ ʏᴏᴜʀ ɢʀᴏᴜᴘ ꜰʀᴏᴍ ᴄᴏᴘʏʀɪɢʜᴛ ɪɴꜰʀɪɴɢᴇᴍᴇɴᴛ ɪ ꜱᴜɢɢᴇꜱᴛᴇᴅ ʏᴏᴜ ᴛᴏ ᴀᴅᴅ ᴍᴇ ɪɴᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ.",
 
   reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "✚ Add me to your Group", url=f"https://t.me/Handlerprobot?startgroup=true")
+                        "✚ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ", url=f"https://t.me/Handlerprobot?startgroup=true")
                 ]          
             ]
        )

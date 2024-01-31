d211cf4f193e534da4669e536464153b05b7a765
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 16:58:16 +0530

    Update start.py

--- RestrictedBot/plugins/start.py
+++ RestrictedBot/plugins/start.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from RestrictedBot import app, LOG_GROUP_ID
+from RestrictedBot import app
 
 
 
@@ -23,13 +23,3 @@ async def start_command(client, message: Message):
             ]
        ),
   )
-
-
-@app.on_message(filters.private)
-async def start_bot_info(_, message: Message):
-    if (await app.get_me()):
-                sender_id = message.from_user.id
-                sender_name = message.from_user.first_name
-                return await app.send_message(
-                    LOG_GROUP_ID,
-                    f"**✫** <b><u>ꜱᴛᴀʀᴛ ɪɴꜰᴏ</u></b> **:**\n\n {message.from_user.mention} ʜᴀꜱ ᴊᴜꜱᴛ ꜱᴛᴀʀᴛᴇᴅ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ ʙᴏᴛ ɪɴꜰᴏ\n\n**ᴜꜱᴇʀ ɪᴅ:** {sender_id}\n**ᴜꜱᴇʀ ɴᴀᴍᴇ:** {sender_name}")

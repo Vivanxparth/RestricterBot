8f76efa3da1a9e6e7288dec9be7cd93985ca6722
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 15:07:28 +0530

    Update start.py

--- RestrictedBot/plugins/start.py
+++ RestrictedBot/plugins/start.py
@@ -5,13 +5,13 @@ from RestrictedBot import app
 @app.on_message(filters.command("start"))
 async def hello(app, message):
   await message.reply_photo(photo=f"https://telegra.ph/file/2938c50c7c6201bbd839e.jpg",
-                              caption=f"Hey, {message.from_user.first_name},\n\n๏ This is a most powerful telegram bot it has a ability to delete unwanted or u can say copyright content it will clean those messages with in a second's if you want to save ⚠️ your group from copyright infringement I suggested you to add me into your group.",
+                              caption=f"Hey, {message.from_user.first_name}\n\n๏ This is a most powerful telegram bot it has a ability to delete unwanted or u can say copyright content it will clean those messages with in a second's if you want to save ⚠️ your group from copyright infringement I suggested you to add me into your group.",
 
   reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "⋆ᴏᴡɴᴇʀ⋆", url=f"https://t.me/bikashbikash")
+                        "✚ Add me to your Group", url=f"https://t.me/Handlerprobot?startgroup=true")
                 ]          
             ]
        ),

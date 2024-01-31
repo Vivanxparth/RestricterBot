efc215a1fda51d6778543bbe3da3e8ba4fdda289
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 19:18:52 +0530

    Create broadcast.py

---
+++ RestrictedBot/plugins/broadcast.py
@@ -0,0 +1,31 @@
+from pyrogram import Client, filters
+from RestrictedBot import app as bot
+
+
+@bot.on_message(filters.command(['start']))
+def send_welcome(message):
+    bot.reply_text(message, "Welcome to the Broadcast Bot!")
+
+@bot.on_message(filters.command(['broadcast']))
+def broadcast_message(client, message):
+    if message.chat.type == 'private':
+        bot.reply_text(message, "This command can only be used in a group or channel.")
+        return
+
+    # Get the message to broadcast
+    broadcast_text = message.text[11:]  # Remove '/broadcast ' from the command
+
+    # Get the chat ID of the group or channel
+    chat_id = message.chat.id
+
+    # Get all members of the group or channel
+    members = bot.get_chat_members_count(chat_id)
+
+    # Send the message to each member
+    for i in range(members):
+        try:
+            bot.send_message(chat_id, broadcast_text)
+        except Exception as e:
+            print(e)
+
+    bot.reply_text(message, "Broadcast sent successfully!")

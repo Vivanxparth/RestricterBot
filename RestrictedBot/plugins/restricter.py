62600f2c2e75641d99c98263d2d886b3992a3e26
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 14:38:14 +0530

    Update restricter.py

--- RestrictedBot/plugins/restricter.py
+++ RestrictedBot/plugins/restricter.py
@@ -14,7 +14,7 @@ max_message_length = 50  # Adjust the maximum length as needed
 
 
 # Event handler to delete long messages
-@app.on_message(filters.text)
+@app.on_message(filters.text & filters.group)
 async def delete_long_messages(client, message):
     if len(message.text) > max_message_length:
         await client.delete_messages(message.chat.id, message.id)
@@ -27,7 +27,7 @@ async def delete_edited_messages(client, message):
 
 
 # Delete messages with files
-@app.on_message(filters.document)
+@app.on_message(filters.document & filters.group)
 async def delete_file_messages(client, message):
     await client.delete_messages(message.chat.id, message.id)
 
@@ -38,7 +38,7 @@ bio_link_pattern = re.compile(r'(http|https)://[^\s]+')
 
 
 # Delete messages with bio links
-@app.on_message(filters.text & filters.private)
+@app.on_message(filters.text & filters.group)
 async def delete_bio_link_messages(client, message):
     if bio_link_pattern.search(message.text):
         await client.delete_messages(message.chat.id, message.id)
@@ -51,45 +51,45 @@ async def delete_photo_messages(client, message):
 
 
 # Delete messages with video
-@app.on_message(filters.video)
+@app.on_message(filters.video & filters.group)
 async def delete_video_messages(client, message):
     await client.delete_messages(message.chat.id, message.id)
 
 
 
 # Delete channel messages
-@app.on_message(filters.channel)
+@app.on_message(filters.channel & filters.group)
 async def delete_channel_messages(client, message):
     await client.delete_messages(message.chat.id, message.id)
 
 
 # Delete messages with audio
-@app.on_message(filters.audio)
+@app.on_message(filters.audio & filters.group)
 async def delete_audio_messages(client, message):
     await client.delete_messages(message.chat.id, message.id)
 
 
 # Delete messages with forwarded 
-@app.on_message(filters.forwarded)
+@app.on_message(filters.forwarded & filters.group)
 async def delete_forwarded_messages(client, message):
     await client.delete_messages(message.chat.id, message.id)
 
 
 # Delete messages with animation
-@app.on_message(filters.animation)
+@app.on_message(filters.animation & filters.group)
 async def delete_file_messages(client, message):
     await client.delete_messages(message.chat.id, message.id)
 
 
 
 # Delete messages with voice note
-@app.on_message(filters.voice)
+@app.on_message(filters.voice & filters.group)
 async def delete_voice_messages(client, message):
     await client.delete_messages(message.chat.id, message.id)
 
 
 # Delete messages with video note
-@app.on_message(filters.video_note)
+@app.on_message(filters.video_note & filters.group)
 async def delete_video_messages(client, message):
     await client.delete_messages(message.chat.id, message.id)
 

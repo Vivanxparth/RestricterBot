cfeb1c82f6838e64261b62e40bbfe04569c797e1
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 14:25:04 +0530

    Update start.py

--- RestrictedBot/plugins/start.py
+++ RestrictedBot/plugins/start.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 from RestrictedBot import app
 
 @app.on_message(filters.command("start"))
-  async def hello(client, message):
+async def hello(client, message):
   await message.reply_photo(photo=f"https://telegra.ph/file/2938c50c7c6201bbd839e.jpg",
                               caption=f"Hey, {message.from_user.first_name},\n\n๏ This is a most powerful telegram bot it has a ability to delete unwanted or u can say copyright content it will clean those messages with in a second's if you want to save ⚠️ your group from copyright infringement I suggested you to add me into your group.",
 

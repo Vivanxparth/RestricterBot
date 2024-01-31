df09d7cef559722df1975f64902eb424e149e5c0
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 17:03:50 +0530

    Create info.py

---
+++ RestrictedBot/plugins/info.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from RestrictedBot import app, LOG_GROUP_ID
+
+
+@app.on_message(filters.private)
+async def start_bot_info(_, message: Message):
+    if (await app.get_me()):
+                sender_id = message.from_user.id
+                sender_name = message.from_user.first_name
+                return await app.send_message(
+                    LOG_GROUP_ID,
+                    f"**✫** <b><u>ꜱᴛᴀʀᴛ ɪɴꜰᴏ</u></b> **:**\n\n {message.from_user.mention} ʜᴀꜱ ᴊᴜꜱᴛ ꜱᴛᴀʀᴛᴇᴅ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ ʙᴏᴛ ɪɴꜰᴏ\n\n**ᴜꜱᴇʀ ɪᴅ:** {sender_id}\n**ᴜꜱᴇʀ ɴᴀᴍᴇ:** {sender_name}")

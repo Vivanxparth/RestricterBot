91126a57dfd57060ffb7c8c2225327515930a0fd
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 18:45:43 +0530

    Update start.py

--- RestrictedBot/plugins/start.py
+++ RestrictedBot/plugins/start.py
@@ -1,4 +1,3 @@
-import logging
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from RestrictedBot import app, LOG_GROUP_ID
@@ -17,15 +16,8 @@ async def start_command(client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✚ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ", url=f"https://t.me/Handlerprobot?startgroup=true"),
-                ],         
+                        "✚ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ", url=f"https://t.me/Handlerprobot?startgroup=true")
+                ]         
             ]
        )
   )
-    
-  await client.send_message(
-                LOG_GROUP_ID,
-                f"**BOT INFO**: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) started @{BOT_USERNAME} !!",
-            )
-        else:
-            logging.info(f"**BOT INFO** :- Name : {message.from_user.first_name} ID : {message.from_user.id}")

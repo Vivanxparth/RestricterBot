791a01f4304605502041adc35c735591e4233748
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 15:14:33 +0530

    Update start.py

--- RestrictedBot/plugins/start.py
+++ RestrictedBot/plugins/start.py
@@ -2,10 +2,11 @@ from pyrogram import *
 from pyrogram.types import *
 from RestrictedBot import app
 
-@app.on_message(filters.command("start"))
+@app.on_message(filters.command("start") & filters.private)
 async def hello(app, message):
-  await message.reply_photo(photo=f"https://telegra.ph/file/2938c50c7c6201bbd839e.jpg",
-                              caption=f"Hey, {message.from_user.mention}\n\n๏ This is a most powerful telegram bot it has a ability to delete unwanted or u can say copyright content it will clean those messages with in a second's if you want to save ⚠️ your group from copyright infringement I suggested you to add me into your group.",
+  await message.reply_photo(
+                        photo=f"https://telegra.ph/file/1912e94ef5c9b2985ec8c.jpg",
+                        caption=f"Hey, {message.from_user.mention}\n\n๏ This is a most powerful telegram bot it has a ability to delete unwanted or u can say copyright content it will clean those messages with in a second's if you want to save ⚠️ your group from copyright infringement I suggested you to add me into your group.",
 
   reply_markup=InlineKeyboardMarkup(
             [
@@ -14,5 +15,5 @@ async def hello(app, message):
                         "✚ Add me to your Group", url=f"https://t.me/Handlerprobot?startgroup=true")
                 ]          
             ]
-       ),
+       )
   )

21139a1a28b06c7c6593971800dd5890421eaff4
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-01-31 16:50:08 +0530

    Update start.py

--- RestrictedBot/plugins/start.py
+++ RestrictedBot/plugins/start.py
@@ -1,5 +1,5 @@
-from pyrogram import *
-from pyrogram.types import *
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from RestrictedBot import app
 
 
@@ -7,6 +7,7 @@ from RestrictedBot import app
 @app.on_message(
     filters.command("start") 
     & filters.incoming
+    & filters.private
 )
 async def start_command(client, message: Message):
   await message.reply_photo(
@@ -22,3 +23,13 @@ async def start_command(client, message: Message):
             ]
        ),
   )
+
+
+@app.on_message(filters.private)
+async def start_bot_info(_, message: Message):
+    if (await app.get_me()):
+                sender_id = message.from_user.id
+                sender_name = message.from_user.first_name
+                return await app.send_message(
+                    LOG_GROUP_ID,
+                    f"**✫** <b><u>ꜱᴛᴀʀᴛ ɪɴꜰᴏ</u></b> **:**\n\n {message.from_user.mention} ʜᴀꜱ ᴊᴜꜱᴛ ꜱᴛᴀʀᴛᴇᴅ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ ʙᴏᴛ ɪɴꜰᴏ\n\n**ᴜꜱᴇʀ ɪᴅ:** {sender_id}\n**ᴜꜱᴇʀ ɴᴀᴍᴇ:** {sender_name}")

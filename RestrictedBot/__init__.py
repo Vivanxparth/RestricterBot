4e6646018c619d3090124c7e3bd3418b2b01f114
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-02-01 17:01:30 +0530

    Update __init__.py

--- RestrictedBot/__init__.py
+++ RestrictedBot/__init__.py
@@ -45,32 +45,9 @@ class app(Client):
         await super().stop()
 
 
-
-
-def install_req(cmd: str) -> Tuple[str, str, int, int]:
-    async def install_requirements():
-        args = shlex.split(cmd)
-        process = await asyncio.create_subprocess_exec(
-            *args,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        stdout, stderr = await process.communicate()
-        return (
-            stdout.decode("utf-8", "replace").strip(),
-            stderr.decode("utf-8", "replace").strip(),
-            process.returncode,
-            process.pid,
-        )
-
-    return asyncio.get_event_loop().run_until_complete(
-        install_requirements()
-    )
-
-
 def git():
         UPSTREAM_REPO = UPSTREAM_REPO
-     try:
+   try:
         repo = Repo()
         LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
     except GitCommandError:

2597f2c44e739ab1d78919dad4a96312a7046ad9
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-02-01 17:56:54 +0530

    Update speedtest.py

--- RestrictedBot/plugins/speedtest.py
+++ RestrictedBot/plugins/speedtest.py
@@ -1,22 +1,46 @@
+import asyncio
 import speedtest
-from pyrogram import Client, filters
+from pyrogram import filters
 from RestrictedBot import app, OWNER_ID
-from pyrogram.types import Message
 
-speedtester = speedtest.Speedtest()
 
-# Define the speedtest command
-@app.on_message(filters.command("speedtest"))
-async def run_speedtest(client, message: Message):
-    # Check if the user is the bot owner
-    if message.from_user.id == OWNER_ID:
-      command, *text = message.text.split(maxsplit=1)
-      text = text[0]
-      speedtester.get_best_server()
-      download_speed = speedtester.download()
-      upload_speed = speedtester.upload()
-      ping = speedtester.results.ping
 
-    speedtest_result = f"Download Speed: {download_speed / 1024 / 1024:.2f} Mbps\nUpload Speed: {upload_speed / 1024 / 1024:.2f} Mbps\nPing: {ping:.2f} ms"
-    await message.reply_text(speedtest_result)
+def testspeed(m):
+    try:
+        test = speedtest.Speedtest()
+        test.get_best_server()
+        m = m.edit("Running Download SpeedTest")
+        test.download()
+        m = m.edit("Running Upload SpeedTest")
+        test.upload()
+        test.results.share()
+        result = test.results.dict()
+        m = m.edit("Sharing SpeedTest Results")
+    except Exception as e:
+        return m.edit(e)
+    return result
 
+
+@app.on_message(filters.command("speedtest") & OWNER_ID)
+async def speedtest_function(client, message):
+    m = await message.reply_text("Running Speed test")
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(None, testspeed, m)
+    output = f"""**Speedtest Results**
+    
+<u>**Client:**</u>
+**__ISP:__** {result['client']['isp']}
+**__Country:__** {result['client']['country']}
+  
+<u>**Server:**</u>
+**__Name:__** {result['server']['name']}
+**__Country:__** {result['server']['country']}, {result['server']['cc']}
+**__Sponsor:__** {result['server']['sponsor']}
+**__Latency:__** {result['server']['latency']}  
+**__Ping:__** {result['ping']}"""
+    msg = await app.send_photo(
+        chat_id=message.chat.id, 
+        photo=result["share"], 
+        caption=output
+    )
+    await m.delete()

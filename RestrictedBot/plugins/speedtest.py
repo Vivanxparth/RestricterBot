65dda57da9a43450433ab002c86e0f49f8775711
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-02-01 14:29:02 +0530

    Update speedtest.py

--- RestrictedBot/plugins/speedtest.py
+++ RestrictedBot/plugins/speedtest.py
@@ -8,11 +8,13 @@ speedtester = speedtest.Speedtest()
 @app.on_message(filters.command("speedtest") & filters.group & filters.private)
 async def run_speedtest(client, message):
     # Check if the user is the bot owner
-     if message.from_user.id == OWNER_ID:
-    speedtester.get_best_server()
-    download_speed = speedtester.download()
-    upload_speed = speedtester.upload()
-    ping = speedtester.results.ping
+    if message.from_user.id == OWNER_ID:
+      command, *text = message.text.split(maxsplit=1)
+      text = text[0]
+      speedtester.get_best_server()
+      download_speed = speedtester.download()
+      upload_speed = speedtester.upload()
+      ping = speedtester.results.ping
 
     speedtest_result = f"Download Speed: {download_speed / 1024 / 1024:.2f} Mbps\nUpload Speed: {upload_speed / 1024 / 1024:.2f} Mbps\nPing: {ping:.2f} ms"
     await message.reply_text(speedtest_result)

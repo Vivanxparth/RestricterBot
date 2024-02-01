79217d27cd8d98c06431ebea537cb37127c9c5b7
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-02-01 17:13:07 +0530

    Update __init__.py

--- RestrictedBot/__init__.py
+++ RestrictedBot/__init__.py
@@ -1,12 +1,9 @@
 import asyncio
-import shlex
-from typing import Tuple
-from git import Repo
-from git.exc import GitCommandError, InvalidGitRepositoryError
 import os
 from os import getenv
 import logging
 from pyrogram import Client
+from RestrictedBot.plugins import git
 
 logging.basicConfig(
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -45,39 +42,5 @@ class app(Client):
         await super().stop()
 
 
-def git():
-        UPSTREAM_REPO = UPSTREAM_REPO
-   try:
-        repo = Repo()
-        LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
-    except GitCommandError:
-        LOGGER(__name__).info(f"Invalid Git Command")
-    except InvalidGitRepositoryError:
-        repo = Repo.init()
-        if "origin" in repo.remotes:
-            origin = repo.remote("origin")
-        else:
-            origin = repo.create_remote("origin", UPSTREAM_REPO)
-        origin.fetch()
-        repo.create_head(
-            config.UPSTREAM_BRANCH,
-            origin.refs[UPSTREAM_BRANCH],
-        )
-        repo.heads[UPSTREAM_BRANCH].set_tracking_branch(
-            origin.refs[UPSTREAM_BRANCH]
-        )
-        repo.heads[UPSTREAM_BRANCH].checkout(True)
-        try:
-            repo.create_remote("origin", UPSTREAM_REPO)
-        except BaseException:
-            pass
-        nrs = repo.remote("origin")
-        nrs.fetch(UPSTREAM_BRANCH)
-        try:
-            nrs.pull(UPSTREAM_BRANCH)
-        except GitCommandError:
-            repo.git.reset("--hard", "FETCH_HEAD")
-        install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGGER(__name__).info(f"Fetched Updates from: {REPO_LINK}")
 
 app = app()

3c11d0881040d9c132adbf50231ed3fe1ab5b027
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-02-01 16:46:40 +0530

    Update __init__.py

--- RestrictedBot/__init__.py
+++ RestrictedBot/__init__.py
@@ -1,3 +1,8 @@
+import asyncio
+import shlex
+from typing import Tuple
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
 import os
 from os import getenv
 import logging
@@ -17,6 +22,8 @@ API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", ""))
 OWNER_ID = int(os.getenv("OWNER_ID", ""))
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/Vivanxparth/RestricterBot")
+UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "master")
                
 class app(Client):
     def __init__(self):
@@ -37,5 +44,63 @@ class app(Client):
     async def stop(self):
         await super().stop()
 
+
+
+
+def install_req(cmd: str) -> Tuple[str, str, int, int]:
+    async def install_requirements():
+        args = shlex.split(cmd)
+        process = await asyncio.create_subprocess_exec(
+            *args,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await process.communicate()
+        return (
+            stdout.decode("utf-8", "replace").strip(),
+            stderr.decode("utf-8", "replace").strip(),
+            process.returncode,
+            process.pid,
+        )
+
+    return asyncio.get_event_loop().run_until_complete(
+        install_requirements()
+    )
+
+
+def git():
+        UPSTREAM_REPO = UPSTREAM_REPO
+    try:
+        repo = Repo()
+        LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
+    except GitCommandError:
+        LOGGER(__name__).info(f"Invalid Git Command")
+    except InvalidGitRepositoryError:
+        repo = Repo.init()
+        if "origin" in repo.remotes:
+            origin = repo.remote("origin")
+        else:
+            origin = repo.create_remote("origin", UPSTREAM_REPO)
+        origin.fetch()
+        repo.create_head(
+            config.UPSTREAM_BRANCH,
+            origin.refs[UPSTREAM_BRANCH],
+        )
+        repo.heads[UPSTREAM_BRANCH].set_tracking_branch(
+            origin.refs[UPSTREAM_BRANCH]
+        )
+        repo.heads[UPSTREAM_BRANCH].checkout(True)
+        try:
+            repo.create_remote("origin", UPSTREAM_REPO)
+        except BaseException:
+            pass
+        nrs = repo.remote("origin")
+        nrs.fetch(UPSTREAM_BRANCH)
+        try:
+            nrs.pull(UPSTREAM_BRANCH)
+        except GitCommandError:
+            repo.git.reset("--hard", "FETCH_HEAD")
+        install_req("pip3 install --no-cache-dir -r requirements.txt")
+        LOGGER(__name__).info(f"Fetched Updates from: {REPO_LINK}")
 
 app = app()

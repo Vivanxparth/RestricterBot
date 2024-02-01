cf5e055f911de82492778a4f72aca40e94102121
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-02-01 18:13:51 +0530

    Update speedtest.py

--- RestrictedBot/plugins/speedtest.py
+++ RestrictedBot/plugins/speedtest.py
@@ -6,26 +6,14 @@ from RestrictedBot import app
 
 
 async def testspeed(m):
-    try:
-        test = speedtest.Speedtest()
-        test.get_best_server()
-        m = m.edit("Running Download SpeedTest")
-        test.download()
-        m = m.edit("Running Upload SpeedTest")
-        test.upload()
-        test.results.share()
-        result = test.results.dict()
-        m = m.edit("Sharing SpeedTest Results")
-    except Exception as e:
-        return m.edit(e)
-    return result
+
 
 
 @app.on_message(filters.command("speedtest"))
 async def speedtest_function(client, message):
     m = await message.reply_text("Running Speed test")
     loop = asyncio.get_event_loop()
-    result = await loop.run_in_executor(None, testspeed, m)
+    result = await loop.run_in_executor(None)
     output = f"""**Speedtest Results**
     
 <u>**Client:**</u>

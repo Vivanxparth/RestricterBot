c208339ec5f7a0b34334eb3f4a579510c4774949
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-02-01 14:35:21 +0530

    Create telegraph.py

---
+++ RestrictedBot/plugins/telegraph.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+import telegraph
+
+# Initialize the Telegraph client
+telegraph_client = telegraph.Telegraph()
+
+# handler for the /tl command
+@app.on_message(filters.command("tl"))
+def new_page_command(client, message):
+    # Create a new page on Telegraph
+    response = telegraph_client.create_page(
+        title="Sample Page",
+        content=["<p>Hello, this is a sample page created by the bot!</p>"]
+    )
+    telegraph_url = "https://graph.org/{}".format(response["path"])
+
+    # Send the Telegraph URL as a reply
+    message.reply_text(telegraph_url)

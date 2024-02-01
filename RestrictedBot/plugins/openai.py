614ad1a4ba24962053ccdf7e7cee124f0252aefd
Vivanxparth <158141812+Vivanxparth@users.noreply.github.com>
2024-02-01 16:05:34 +0530

    Update openai.py

--- RestrictedBot/plugins/openai.py
+++ RestrictedBot/plugins/openai.py
@@ -1,23 +1,24 @@
 import requests
-from RestrictedBot import app
+from RestrictedBot import app as bot
 import time
 from pyrogram.enums import ChatAction, ParseMode
-from pyrogram import filters, Client
+from pyrogram import filters
 
-@app.on_message(filters.command(["ask","gpt"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
-async def chat(client, message):
+@bot.on_message(filters.command("ask"))
+async def chat(bot, message):
+    
     try:
         start_time = time.time()
-        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
         if len(message.command) < 2:
             await message.reply_text(
-            "Example:**\n\n/ask Who are you?")
+            "❍ Example ➛ /ask ᴡʜᴇʀᴇ ɪs ᴛᴀᴊᴍᴀʜᴀʟ ?")
         else:
             a = message.text.split(' ', 1)[1]
             response = requests.get(f'https://mukesh-api.vercel.app/chatgpt/{a}') 
             x=response.json()["results"]
             end_time = time.time()
             telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ᴍs"
-            await message.reply_text(f" {x}\n\n✨ᴛɪᴍᴇ ᴛᴀᴋᴇɴ  {telegram_ping} \n\n 💓 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ", parse_mode=ParseMode.MARKDOWN)     
+            await message.reply_text(f"❍ {x}\n\n❍ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➛ ๛ᴀ ᴠ ɪ s ʜ ᴀ ༗ ", parse_mode=ParseMode.MARKDOWN)     
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
